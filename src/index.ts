cae4213d6b34cfd3aae5f4a1a585f6d5f35c4634
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2026-05-09 21:40:35 +0500

    Converted Tokenizer to Object

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { TokenType, tokenize } from "./tokenizer.js";
+import { Token, TokenType, Tokenizer } from "./tokenizer.js";
 
-let tokens = tokenize('5 + 67 + 69 + func(x)');
+let expression = '5 + 67 + 69 + func(x)';
+
+
+let myTokenizer = new Tokenizer();
+let tokens : Array<Token> = myTokenizer.run(expression);
 
 for (let i = 0; i < tokens.length; i++) {
     console.log(`Token: ${tokens[i].value}\t, Type: ${TokenType[tokens[i].type]}`)
--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,8 +1,8 @@
-type Token = {type : TokenType, value : number | string};
+export type Token = { type: TokenType, value: number | string };
 export enum TokenType { Text, Constant, Operator, Dot, BracketOpen, BracketClose, SqBracketOpen, SqBracketClose, CuBracketOpen, CuBracketClose, Invalid }
 
-export function tokenize(expression: string): Array<Token> {
-    function getTokenType(char: string): TokenType {
+export class Tokenizer {
+    getTokenType(char: string): TokenType {
         if (char >= "a" && char <= "z" || char >= "A" && char <= "Z") return TokenType.Text;
         if (char >= "0" && char <= "9") return TokenType.Constant;
         if ("+-*/".includes(char)) return TokenType.Operator;
@@ -16,32 +16,34 @@ export function tokenize(expression: string): Array<Token> {
         return TokenType.Invalid
     }
 
-    function accumulate(type: TokenType): string {
-        let accumulator: string = expression[i];
+    run(expression: string) {
+        const accumulate = (type: TokenType): string => {
+            let accumulator: string = expression[i];
+            for (0; i < expression.length; i++) {
+                if (this.getTokenType(expression[i + 1]) === type) accumulator += expression[i + 1];
+                else break;
+            }
+            return accumulator;
+        }
+
+        let i = 0;
+        let arr: Array<Token> = [];
+
         for (0; i < expression.length; i++) {
-            if (getTokenType(expression[i + 1]) === type) accumulator += expression[i + 1];
-            else break;
+            let char = expression[i];
+            if (char === ' ') continue;
+            let type: TokenType = this.getTokenType(char);
+            switch (type) {
+                case TokenType.Constant:
+                    arr.push({ type: TokenType.Constant, value: parseInt(accumulate(TokenType.Constant)) });
+                    break;
+                case TokenType.Text:
+                    arr.push({ type: TokenType.Text, value: accumulate(TokenType.Text) });
+                    break;
+                default:
+                    arr.push({ type: type, value: char });
+            }
         }
-        return accumulator;
+        return arr;
     }
-
-    let i = 0;
-    let arr: Array<Token> = [];
-
-    for (0; i < expression.length; i++) {
-        let char = expression[i];
-        if (char === ' ') continue;
-        let type: TokenType = getTokenType(char);
-        switch (type) {
-            case TokenType.Constant:
-                arr.push({ type: TokenType.Constant, value: parseInt(accumulate(TokenType.Constant)) });
-                break;
-            case TokenType.Text:
-                arr.push({ type: TokenType.Text, value: accumulate(TokenType.Text) });
-                break;
-            default:
-                arr.push({ type: type, value: char });
-        }
-    }
-    return arr;
 }
